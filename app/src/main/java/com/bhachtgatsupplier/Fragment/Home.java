3863037bffc49c13e0592dd792dcd432aa6ea77e
Amirmulla <amirmulla303@gmail.com>
2020-07-28 12:47:42 +0530

    initial commit

---
+++ app/src/main/java/com/bhachtgatsupplier/Fragment/Home.java
@@ -0,0 +1,54 @@
+package com.bhachtgatsupplier.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bhachtgatsupplier.Activity.MainPage;
+import com.bhachtgatsupplier.Extra.DetectConnection;
+import com.bhachtgatsupplier.R;
+
+import butterknife.ButterKnife;
+import es.dmoral.toasty.Toasty;
+
+
+public class Home extends Fragment {
+
+    View view;
+    public static SwipeRefreshLayout swipeRefreshLayout;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+        MainPage.title.setText("");
+
+        swipeRefreshLayout = view.findViewById(R.id.simpleSwipeRefreshLayout);
+
+        return view;
+
+    }
+
+    public void onStart() {
+        super.onStart();
+        Log.e("onStart", "called");
+        MainPage.title.setVisibility(View.VISIBLE);
+        ((MainPage) getActivity()).lockUnlockDrawer(0);
+        MainPage.drawerLayout.closeDrawers();
+        if (DetectConnection.checkInternetConnection(getActivity())) {
+
+
+        } else {
+            Toasty.warning(getActivity(), "No Internet Connection", Toasty.LENGTH_SHORT, true).show();
+        }
+    }
+}
